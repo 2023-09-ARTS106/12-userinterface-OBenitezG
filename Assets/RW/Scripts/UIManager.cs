4796853685aa7015ce2f21f7454176b07a17eb67
Obdulio Benitez Garcia <112982014+OBenitezG@users.noreply.github.com>
2023-11-15 12:28:18 -0500

    Saving

---
+++ Assets/RW/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Animator startButton;
+    public Animator settingsButton;
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+}
